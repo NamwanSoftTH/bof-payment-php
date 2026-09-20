7b9e5ae5be2ab19a6329c281b83a8716c5c90350
NWSoft <anusit_khuandee@hotmail.com>
2026-09-20 09:33:23 +0700

    Initial commit

---
+++ src/Xendit.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace NamwanSoft\Payment;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+
+class Xendit
+{
+    private $client;
+
+    public function __construct($apiEndpoint, $apiKey)
+    {
+        $this->client = new Client([
+            'base_uri' => rtrim($apiEndpoint, '/') . '/',
+            'timeout'  => 10.0,
+            'headers'  => [
+                'Authorization' => 'Bearer ' . $apiKey,
+                'Accept'        => 'application/json'
+            ]
+        ]);
+    }
+
+    public function createPayment($orderId, $amount)
+    {
+        try {
+            $response = $this->client->post('api/v1/payments', [
+                'json' => [
+                    'order_id' => $orderId,
+                    'amount' => $amount
+                ]
+            ]);
+
+            return [
+                'success' => true,
+                'data' => json_decode($response->getBody(), true)
+            ];
+        } catch (RequestException $e) {
+            return [
+                'success' => false,
+                'message' => $e->getMessage()
+            ];
+        }
+    }
+}
